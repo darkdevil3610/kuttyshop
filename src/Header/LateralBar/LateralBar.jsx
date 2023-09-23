9c9c78aac4ac87258e8a78aa59b00a396c5e541d
dark_devil_3609 <94959564+darkdevil3610@users.noreply.github.com>
2023-09-23 17:12:07 +0530

    Update LateralBar.jsx

--- src/Header/LateralBar/LateralBar.jsx
+++ src/Header/LateralBar/LateralBar.jsx
@@ -19,7 +19,7 @@ function LateralBar({ open, func }) {
         <a className="link navLink underline" href="/shops">Stores</a>
       </li>
       <li className="li">
-        <a className="link navLink underline" href="https://first-page-admin-page.vercel.app/">Admin</a>
+        <a className="link navLink underline" href="https://kuttyshop-admin.vercel.app/">Admin</a>
       </li>
       <button type="button" className="btnExit pointer" onClick={func}>
         <FontAwesomeIcon icon={faX} size="2x" />
